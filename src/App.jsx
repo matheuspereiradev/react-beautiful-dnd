1b3ace00ee5454d330c757ce895060ecb6f15314
Matheus Lima <matheuslima20111997@gmail.com>
2021-08-29 20:59:22 -0300

    criando board e estilizando

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { v4 as uuid } from 'uuid'
 
 function App() {
 
-  const brasileirao = [
+  const brazilAGroup = [
     {
       id: uuid(),
       name: 'america mg',
@@ -26,6 +26,9 @@ function App() {
       name: 'fortaleza',
       thumb: '/fortaleza.png'
     },
+  ]
+
+  const brazilBGroup = [
     {
       id: uuid(),
       name: 'palmeiras',
@@ -36,12 +39,9 @@ function App() {
       name: 'santos',
       thumb: '/santos.png'
     },
-
   ]
 
-
-
-  const europe = [
+  const europeAGroup = [
     {
       id: uuid(),
       name: 'arsenal',
@@ -61,7 +61,11 @@ function App() {
       id: uuid(),
       name: 'juventus',
       thumb: '/juve.png'
-    },
+    }
+
+  ]
+
+  const europeBGroup = [
     {
       id: uuid(),
       name: 'milan',
@@ -82,71 +86,133 @@ function App() {
 
 
 
-  function handleOnDragEnd(result) {
-    if (!result.destination) return;
-    const items = Array.from(brazilianTeam);
-    const [reorderedItem] = items.splice(result.source.index, 1);
-    items.splice(result.destination.index, 0, reorderedItem);
-    setBrazilianTeam(items);
+  const columns = {
+    [uuid()]: {
+      name: "Grupo A",
+      teams: brazilAGroup
+    },
+    [uuid()]: {
+      name: "Grupo B",
+      teams: brazilBGroup
+    }
   }
 
-  const [brazilianTeam, setBrazilianTeam] = useState(brasileirao);
 
+  function handleOnDragEnd(result) {
+    if (!result.destination) return;
+    const { source, destination } = result;
+
+    if (source.droppableId !== destination.droppableId) {
+      const sourceColumn = columnsGroups[source.droppableId];
+      const destColumn = columnsGroups[destination.droppableId];
+      const sourceItems = [...sourceColumn.teams];
+      const destItems = [...destColumn.teams];
+      const [removed] = sourceItems.splice(source.index, 1);
+      destItems.splice(destination.index, 0, removed);
+      setColumnsGroups({
+        ...columnsGroups,
+        [source.droppableId]: {
+          ...sourceColumn,
+          teams: sourceItems
+        },
+        [destination.droppableId]: {
+          ...destColumn,
+          teams: destItems
+        }
+      });
+    } else {
+      const column = columnsGroups[source.droppableId];
+      const copiedItems = [...column.teams];
+      const [removed] = copiedItems.splice(source.index, 1);
+      copiedItems.splice(destination.index, 0, removed);
+      setColumnsGroups({
+        ...columnsGroups,
+        [source.droppableId]: {
+          ...column,
+          teams: copiedItems
+        }
+      });
+    }
+  }
+
+  const [columnsGroups, setColumnsGroups] = useState(columns);
+  console.log(columnsGroups)
   return (
-    <div className="App">
+    <div style={{ display: "flex", justifyContent: "center", height: "100%" }}>
       <DragDropContext onDragEnd={handleOnDragEnd}>
-        <Droppable droppableId="characters" key="characters">
-          {(provided, snapshot) => (
-            <div
-              {...provided.droppableProps}
-              ref={provided.innerRef}
-              style={{
-                background: snapshot.isDraggingOver
-                  ? "lightblue"
-                  : "lightgrey",
-                padding: 4,
-                width: 250,
-                minHeight: 500
-              }}
-            >
-              <h3>Brasileirão</h3>
-              <div className="characters">
-                {brazilianTeam.map(({ id, name, thumb }, index) => {
-                  return (
-                    <Draggable
-                      key={id}
-                      draggableId={id}
-                      index={index}
+        {
+          Object.entries(columnsGroups).map(([groupID, group], index) => {
+            return (
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
+                  margin: 10,
+                  border: '1px black solid'
+                }}
+                key={groupID}
+              >
+                <Droppable droppableId={groupID} key={groupID}>
+                  {(provided, snapshot) => (
+                    <div
+                      {...provided.droppableProps}
+                      ref={provided.innerRef}
+                      style={{
+                        background: snapshot.isDraggingOver
+                          ? "lightblue"
+                          : "#EBECF0",
+                        padding: 4,
+                        width: 250,
+                        minHeight: 500
+                      }}
                     >
-                      {(provided, snapshot) => (
-                        <div
-                          ref={provided.innerRef}
-                          {...provided.draggableProps}
-                          {...provided.dragHandleProps}
-                          style={{
-                            userSelect: "none",
-                            padding: 16,
-                            margin: "0 0 8px 0",
-                            minHeight: "50px",
-                            backgroundColor: snapshot.isDragging
-                              ? "#263B4A"
-                              : "#456C86",
-                            color: "white",
-                            ...provided.draggableProps.style
-                          }}
-                        >
-                          <img src={thumb} alt={name} style={{ width: 20 }} />
-                          {name}
-                        </div>
-                      )
-                      }
-                    </Draggable>
-                  );
-                })}
+                      {/* {provided.placeholder} */}
+                      <h3>{group.name}</h3>
+                      <div className="team">
+                        {console.log(group)}
+                        {group.teams.map(({ id, name, thumb }, index) => {
+                          return (
+                            <Draggable
+                              key={id}
+                              draggableId={id}
+                              index={index}
+                            >
+                              {(provided, snapshot) => (
+                                <div
+                                  ref={provided.innerRef}
+                                  {...provided.draggableProps}
+                                  {...provided.dragHandleProps}
+                                  style={{
+                                    userSelect: "none",
+                                    padding: 8,
+                                    margin: "0 0 8px 0",
+                                    minHeight: "50px",
+                                    backgroundColor: snapshot.isDragging
+                                      ? "#cfcbd3"
+                                      : "#FFFFFF",
+                                    color: "#09335A",
+                                    border: "1px #09335A solid",
+                                    ...provided.draggableProps.style
+                                  }}
+                                >
+                                  <img src={thumb} alt={name} style={{ width: 20 }} />
+                                  {name}
+                                </div>
+                              )
+                              }
+                            </Draggable>
+                          );
+                        })}
+                      </div>
+                    </div>
+                  )}
+                </Droppable>
               </div>
-            </div>
-          )}
-        </Droppable>
+            )
+          })
+        }
+
       </DragDropContext>
     </div >
   );
